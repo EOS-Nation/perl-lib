7665da4191249f4ca12abed2750daecbb1386435
Matthew <matthew@mdarwin.ca>
2020-04-04 10:12:36 -0400

    initial perl libraries

---
+++ EOSN/CommandUtil.pm
@@ -0,0 +1,82 @@
+package EOSN::CommandUtil;
+
+use utf8;
+use strict;
+use Date::Format qw(time2str);
+use File::Slurp qw(read_file write_file read_dir);
+use File::Find;
+
+use parent qw(Exporter);
+our @EXPORT_OK = qw(write_timestamp_log find_dirs write_file_atomic clean_old_files);
+
+# --------------------------------------------------------------------------
+# Subroutines
+
+sub write_timestamp_log {
+	my ($key, $value) = @_;
+
+	my $log = $key;
+	if (defined $value) {
+		$log = "$key: $value";
+	}
+
+	if ($ENV{INVOCATION_ID}) {
+		# running under systemd
+		$| = 1;
+		print sprintf ("%s\n", $log);
+	} else {
+		print sprintf ("%s: %s\n", time2str ("%Y-%m-%d %X", time), $log);
+	}
+}
+
+sub find_dirs {
+	my (@dirs) = @_;
+
+	our @directories;
+
+	find (\&wanted, @dirs);
+
+	sub wanted {
+		return if (! -d $_);
+
+		my $x = $File::Find::name;
+		$x =~ s#^\./##;
+		push (@directories, $x);
+	}
+
+	return @directories;
+}
+
+sub write_file_atomic {
+	my ($filename, @stuff) = @_;
+
+	my $temp = "$filename.tmp";
+	write_file ($temp, @stuff);
+	rename ($temp, $filename);
+}
+
+sub clean_old_files {
+	my (%options) = @_;
+
+	my $dir = $options{dir} || die;
+	my $days = $options{days} || die;
+	my $message = $options{message} || 'clean old files';
+
+	write_timestamp_log ("$message starting");
+
+	my @files = read_dir ($dir);
+	foreach my $file (sort @files) {
+		next if (! -f "$dir/$file");
+		my $mtime = (stat ("$dir/$file"))[9];
+		my $age = sprintf ("%.1f", (time - $mtime) / 3600 / 24);
+
+		if ($age > $days) {
+			write_timestamp_log ("remove file=<$file> age=<$age days>");
+			unlink ("$dir/$file");
+		}
+	}
+
+	write_timestamp_log ("$message done");
+}
+
+1;
---
+++ EOSN/SQL.pm
@@ -0,0 +1,75 @@
+package EOSN::SQL;
+
+# --------------------------------------------------------------------------
+
+use utf8;
+use strict;
+use Carp;
+use DBI;
+use Exporter;
+
+use parent qw (Exporter);
+our @EXPORT_OK = qw (do_connect_mysql do_connect_sqlite);
+
+# --------------------------------------------------------------------------
+# Subroutines
+
+sub do_connect_mysql {
+	my ($database, $user, $password, $host) = @_;
+
+	if (! $database) {
+		croak "No database specified";
+	}
+
+	my $dbh;
+	my $i = 0;
+	my $env_prefix = "MYSQL_DATABASE_" . uc($database);
+	my $env_user = "${env_prefix}_USER";
+	my $env_pass = "${env_prefix}_PASS";
+	my $env_host = "${env_prefix}_HOST";
+
+	if (! defined $user) {
+		$user = $ENV{$env_user} || die "$0: $env_user is not defined";
+	}
+
+	if (! defined $password) {
+		$password = $ENV{$env_pass} || die "$0: $env_pass is not defined";
+	}
+
+	if (! defined $host) {
+		$host = $ENV{$env_host} || die "$0: $env_host is not defined";
+	}
+
+	while (! $dbh) {
+		$i++;
+		if ($i > 10) {
+			croak "Cannot connect to the database: $!";
+		}
+		$dbh = DBI->connect("dbi:mysql:dbname=$database;host=$host", $user, $password, {mysql_enable_utf8 => 1, mysql_enable_utf8mb4 => 1});
+		if (! $dbh) {
+			warn "connect failed (attempt $i): " . $DBI::errstr . "\n";
+			sleep 60;       # wait for the database to start up
+		}
+	}
+
+	return $dbh;
+}
+
+sub do_connect_sqlite {
+	my ($database) = @_;
+
+	if (! $database) {
+		croak "No database specified";
+	}
+
+	my $user = "";
+	my $password = "";
+	my $dbh = DBI->connect("dbi:SQLite:dbname=/var/lib/sqlite/$database.sqlite", $user, $password, {mysql_enable_utf8 => 1, mysql_enable_utf8mb4 => 1});
+	if (! $dbh) {
+		croak "connect failed " . $DBI::errstr . "\n";
+	}
+
+	return $dbh;
+}
+
+1;
