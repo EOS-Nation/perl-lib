ea66db845109d8aa5321905f600c9af9f6f8cc68
Matthew <matthew@mdarwin.ca>
2021-04-26 12:11:13 -0400

    add new function: find_dirs_notdot()

--- EOSN/FindDirs.pm
+++ EOSN/FindDirs.pm
@@ -5,7 +5,7 @@ use strict;
 use File::Find;
 
 use parent qw(Exporter);
-our @EXPORT_OK = qw(find_dirs);
+our @EXPORT_OK = qw(find_dirs find_dirs_notdot);
 
 # --------------------------------------------------------------------------
 # Subroutines
@@ -15,15 +15,30 @@ sub find_dirs {
 
 	our @directories;
 
-	find (\&wanted, @dirs);
-
-	sub wanted {
+	find (sub {
 		return if (! -d $_);
 
 		my $x = $File::Find::name;
 		$x =~ s#^\./##;
 		push (@directories, $x);
-	}
+	}, @dirs);
+
+	return @directories;
+}
+
+sub find_dirs_notdot {
+	my (@dirs) = @_;
+
+	our @directories;
+
+	find (sub {
+		return if (! -d $_);
+		return if ($_ =~ /^\./);
+
+		my $x = $File::Find::name;
+		$x =~ s#^\./##;
+		push (@directories, $x);
+	}, @dirs);
 
 	return @directories;
 }
